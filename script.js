fcd72a666785e0cf285f3278feb0267bfc7ab8cf
Piyush Vijay <Piyush Vijay>
2017-01-18 03:45:35 +0530

    final game

--- script.js
+++ script.js
@@ -1,19 +1,111 @@
 $(function(){
-	$('#turn').text('Player 1 turn');
-	$('.col-md-4').click(function(){
-		var initial_check = $(this).children().text();
-		var turn = $('#turn').text();
-		if(initial_check == ""){
-			$(this).children().text("X");
+	$('.X').click(function(){
+		var x = $(this).attr('value');
+		if(x == "X"){
+			var y = "O";
 		}
-		else if(initial_check == "X"){
-			$(this).children().text("");
+		else if(x == "O"){
+			var y = "X";
 		}
-		if(turn == "Player 1 turn"){
-			$('#turn').text("Player 2 turn");
+		$('.container').css('display', 'block');
+		$('form').css('display', 'none');
+		if(x == "X"){
+			$('#player_1').text('Player 1 is '+' : '+ ' '+' '+x);
+			$('#player_2').text('Player 2 is '+' : '+ ' '+' '+'O');
 		}
-		else if(turn == "Player 2 turn"){
-			$('#turn').text('Player 1 turn');
+		else{
+			$('#player_1').text('Player 1 is '+' : '+ ' '+' '+x);
+			$('#player_2').text('Player 2 is '+' : '+ ' '+' '+'X');
 		}
+		$('#turn').text('Player 1 turn');
+		$('.col-md-4').click(function(){
+			$(this).parent().css('pointer-events', 'none');
+			$(this).parent().css('cursor', 'default');
+			var initial_check = $(this).children().text();
+			var turn = $('#turn').text();
+			if(initial_check == ""){
+				if($('#turn').text() == "Player 1 turn" && x=="X"){
+					$(this).children().text("X");
+				}
+				else if($('#turn').text() == "Player 1 turn" && x=="O"){
+					$(this).children().text("O");
+				}
+				else if($('#turn').text() == "Player 2 turn" && x=="X"){
+					$(this).children().text("O");
+				}
+				else if($('#turn').text() == "Player 2 turn" && x=="O"){
+					$(this).children().text("X");
+				}
+				if(turn == "Player 1 turn"){
+					$('#turn').text("Player 2 turn");
+				}
+				else if(turn == "Player 2 turn"){
+					$('#turn').text('Player 1 turn');
+				}
+			}
+			else if(initial_check == "X"){
+				$(this).children().text("");
+			}
+			else if(initial_check == "O"){
+				$(this).children().text("");
+			}
+			var array = [];
+			$('.col-md-4').each(function(){
+				array.push($(this).children().text());
+			});
+			console.log(array);
+			var length = 0;
+			for(var i=0;i<=8;i++){
+				if(array[i] != ""){
+					length++;
+				}
+			}
+			console.log(length);
+			for(var j=0;j<9;j+=3){
+				if(array[j]==x && array[j+1]==x && array[j+2]==x){
+					alert("Player 1 wins the game");
+					window.location = "index.html";
+				}
+				else if(array[j]==y && array[j+1]==y && array[j+2]==y){
+					alert("Player 2 wins the game");
+					window.location = "index.html";
+				}
+			}
+			for(var j=0;j<3;j++){
+				if(array[j]==x && array[j+3]==x && array[j+6]==x){
+					alert("Player 1 wins the game");
+					window.location = "index.html";
+				}
+				else if(array[j]==y && array[j+3]==y && array[j+6]==y){
+					alert("Player 2 wins the game");
+					window.location = "index.html";
+				}
+			}
+			for(var j=0;j<1;j++){
+				if(array[j]==x && array[j+4]==x && array[j+8]==x){
+					alert("Player 1 wins the game");
+					window.location = "index.html";
+				}
+				else if(array[j]==y && array[j+4]==y && array[j+8]==y){
+					alert("Player 2 wins the game");
+					window.location = "index.html";
+				}
+			}
+			for(var j=2;j<3;j++){
+				if(array[j]==x && array[j+2]==x && array[j+2]==x){
+					alert("Player 1 wins the game");
+					window.location = "index.html";
+				}
+				else if(array[j]==y && array[j+2]==y && array[j+2]==y){
+					alert("Player 2 wins the game");
+					window.location = "index.html";
+				}
+			}
+			if(length == 9){
+				alert("The game is draw between you two. Try again...");
+				window.location = "index.html";
+			}
+		});
 	});
+	
 });
